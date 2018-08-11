8c787140bfaa93a4ae18c968c4a6d296a32df977
Peter Kang <Peterrkang@gmail.com>
2018-08-11 15:16:14 -0700

    add expect library

--- utils/utils.js
+++ utils/utils.js
@@ -2,9 +2,31 @@ const add = (a, b) => {
   return a + b;
 };
 
+const asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
+
 const square = x => x * x;
+
+const asyncSquare = (n, callback) => {
+  setTimeout(() => {
+    callback(n * n);
+  }, 1000);
+};
+
+const setName = (user, fullName) => {
+  var name = fullName.split(" ");
+  user.firstName = name[0];
+  user.lastName = name[1];
+  return user;
+};
 
 module.exports = {
   add,
-  square
+  square,
+  setName,
+  asyncAdd,
+  asyncSquare
 };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,12 +1,43 @@
 //behavior driven dev
 const utils = require("./utils");
+const expect = require("expect");
 
 it("should add two numbers", () => {
   const res = utils.add(33, 11);
-  if (res !== 44) throw new Error(`Expected 44, but got ${res}`);
+  expect(res)
+    .toBe(44)
+    .toBeA("number");
+});
+
+it("should async add two numbers", done => {
+  utils.asyncAdd(3, 4, sum => {
+    expect(sum)
+      .toEqual(7)
+      .toBeA("number");
+    done();
+  });
 });
 
 it("should square a number", () => {
   const res = utils.square(3);
-  if (res !== 9) throw new Error(`Expected 9 but got ${res}`);
+  expect(res)
+    .toBeA("number")
+    .toEqual(9);
+});
+
+it("should async square a nubmer", done => {
+  utils.asyncSquare(3, res => {
+    expect(res)
+      .toEqual(9)
+      .toBeA("number");
+    done();
+  });
+});
+
+it("should return user name", () => {
+  let user = { age: 23, location: "test" };
+  utils.setName(user, "Peter Kang");
+  expect(user)
+    .toInclude({ firstName: "Peter", lastName: "Kang" })
+    .toBeA("object");
 });
